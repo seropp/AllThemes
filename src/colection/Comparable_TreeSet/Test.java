d5f747e6d9ae74d85a064c3cb72eb84ad951b325
seropp <ts.seropps@gmail.com>
2021-07-25 20:27:03 +0300

    ver3

--- src/colection/Comparable_TreeSet/Test.java
+++ src/colection/Comparable_TreeSet/Test.java
@@ -1,25 +1,34 @@
 package colection.Comparable_TreeSet;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.TreeSet;
 
 public class Test {
+
     public static void main(String[] args) {
 
-        Comparator<People> comp = new PeopleSexComparator().thenComparing(new PeopleAgeComparator());
-        TreeSet<People> treeSet = new TreeSet<People>(comp);
+//        Comparator<People> comp = new PeopleSexComparator();
+//        Comparator<People> comp2 = new PeopleAgeComparator();
+//        TreeSet<People> treeSet = new TreeSet<People>(comp);
+        List<People> treeSet = new ArrayList<People>();
+
         treeSet.add(new People("Toni", 34, Sex.MAN));
         treeSet.add(new People("Kate", 50, Sex.WOMAN));
         treeSet.add(new People("Din", 42, Sex.MAN));
-        treeSet.add(new People("Sofi", 34, Sex.WOMAN));
+        treeSet.add(new People("sofi", 34, Sex.WOMAN));
         treeSet.add(new People("Hanna", 27, Sex.WOMAN));
         treeSet.add(new People("Bill", 60, Sex.MAN));
         treeSet.add(new People("Emmanuel", 20, Sex.MAN));
         treeSet.add(new People("Sergey", 25, Sex.MAN));
-
-        for (People x : treeSet) {
-            System.out.printf("NAME: %S, AGE: %d, SEX: %s%n", x.getName(),x.getAge(),x.getSex());
-        }
+        treeSet.stream()
+                .sorted(Comparator.comparing(People::getSex)
+                .thenComparing((People::getName)))
+                .forEach(x ->System.out.printf("Name: %S, AGE: %d, SEX: %s%n", x.getName(),x.getAge(),x.getSex()));
+//        for (People x : treeSet) {
+//
+//        }
 
 
     }
@@ -52,16 +61,16 @@ class People {
     }
 }
 
-class PeopleSexComparator implements Comparator<People> {
-
-    public int compare(People a, People b){
-        if(a.getSex() == b.getSex()) return 0;
-        else if((a.getSex() == Sex.MAN) && (b.getSex() == Sex.WOMAN)) return -1;
-        return 1;
-    }
-}
-class PeopleAgeComparator implements Comparator<People> {
-    public int compare(People a, People b){
-        return a.getAge()-b.getAge();
-    }
-}
+//class PeopleSexComparator implements Comparator<People> {
+//
+//    public int compare(People a, People b){
+//        if(a.getSex() == b.getSex()) return 0;
+//        else if((a.getSex() == Sex.MAN) && (b.getSex() == Sex.WOMAN)) return -1;
+//        return 1;
+//    }
+//}
+//class PeopleAgeComparator implements Comparator<People> {
+//    public int compare(People a, People b){
+//        return a.getAge()-b.getAge();
+//    }
+//}
