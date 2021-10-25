370243ca6b5d112b636a9e3380aff58cbed8e6bc
seropp <ts.seropps@gmail.com>
2021-10-25 20:03:23 +0300

    ver2510

--- Io-nio/src/HierarchyIO.java
+++ Io-nio/src/HierarchyIO.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HierarchyIO {
     public static void main(String[] args) throws IOException{
@@ -42,37 +44,69 @@ public class HierarchyIO {
 //        PrintStream printStream = new PrintStream(new FileOutputStream("file.txt"));
 //
 //
+//
+//        // 1. Работа с массивами символов (не требуют явного закрытия потока)
+//        CharArrayReader caR = new CharArrayReader(new char[23]);
+//        CharArrayWriter caW = new CharArrayWriter(23);
+//        // 2. sW можно не закрывать. sW инкапсулирует StringBuffer. Через append()
+//        // можем доб. информ к объекту StringBuffer. sR для чтения String в стиле символьного входного поток
+//        // нужно использовать BufferedReader
+//        StringWriter sW = new StringWriter();
+//        StringReader sR = new StringReader(sW.toString());
+//        // 3.Каждый раз, когда данные записываются в PipedWriter, они автоматически появляются в PipedReader
+//        // для этого передаем pW в конструктор pR, или явный вызов метода  -  pR.connect(pW); + Buffered
+//        PipedWriter pW = new PipedWriter();
+//        PipedReader pR = new PipedReader(pW);
+//        // 4. оболочки
+//        BufferedReader bR = new BufferedReader(new FileReader("file.txt"));
+//        BufferedWriter bW = new BufferedWriter(new FileWriter("file.txt"));
+//        // 5. FilterReader это абстрактный подкласс.
+//        FilterReader filterR = new PushbackReader(new FileReader("file.txt"));
+//        // FilterWriter filterW; абстрактный подкласс
+//        // 6. является средством чтения символьного потока, которое позволяет символам возвращаться в поток.
+//        // наследник FilterReader
+//        PushbackReader pbR = new PushbackReader(new FileReader("file.txt"));
+//        // 7. преобразуют байтовые потоки в символьные исп Buffered
+//        InputStreamReader iSR = new InputStreamReader(new FileInputStream("file.txt"));
+//        OutputStreamWriter oSR = new OutputStreamWriter(new FileOutputStream("file.txt"));
+//        // 8. Для работы с текстовыми файлами
+//        FileReader fR = new FileReader(Files.readString(Path.of("A:\\TestFiles\\3file.txt")));
+//        FileWriter fW = new FileWriter("file.txt",true);
+//        // 9. Подкласс Writer, который используется для печати форматированных данных в OutputStream или другой Writer,
+//        // которым он управляет. Все методы PrintWriter не выдают исключений I/O.
+//        PrintWriter printW = new PrintWriter("file.txt");
+//
+//        InputStream input = Files.newInputStream(Path.of(""));
+//        BufferedReader fR = new BufferedReader(new FileReader("A:\\TestFiles\\3file.txt"));
+//        String a;
+//        while ((a = fR.readLine())!=null) {
+//            System.out.println(a);
+//        }
+//            System.in.close();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//            System.out.println(reader.readLine());
+//        PushbackInputStream pushbackInputStream = new PushbackInputStream(System.in);
+//        System.out.println(pushbackInputStream.read());
 
-        // 1. Работа с массивами символов (не требуют явного закрытия потока)
-        CharArrayReader caR = new CharArrayReader(new char[23]);
-        CharArrayWriter caW = new CharArrayWriter(23);
-        // 2. sW можно не закрывать. sW инкапсулирует StringBuffer. Через append()
-        // можем доб. информ к объекту StringBuffer. sR для чтения String в стиле символьного входного поток
-        // нужно использовать BufferedReader
-        StringWriter sW = new StringWriter();
-        StringReader sR = new StringReader(sW.toString());
-        // 3.Каждый раз, когда данные записываются в PipedWriter, они автоматически появляются в PipedReader
-        // для этого передаем pW в конструктор pR, или явный вызов метода  -  pR.connect(pW); + Buffered
-        PipedWriter pW = new PipedWriter();
-        PipedReader pR = new PipedReader(pW);
-        // 4. оболочки
-        BufferedReader bR = new BufferedReader(new FileReader("file.txt"));
-        BufferedWriter bW = new BufferedWriter(new FileWriter("file.txt"));
-        // 5. FilterReader это абстрактный подкласс.
-        FilterReader filterR = new PushbackReader(new FileReader("file.txt"));
-        // FilterWriter filterW; абстрактный подкласс
-        // 6. является средством чтения символьного потока, которое позволяет символам возвращаться в поток.
-        // наследник FilterReader
-        PushbackReader pbR = new PushbackReader(new FileReader("file.txt"));
-        // 7. преобразуют байтовые потоки в символьные исп Buffered
-        InputStreamReader iSR = new InputStreamReader(new FileInputStream("file.txt"));
-        OutputStreamWriter oSR = new OutputStreamWriter(new FileOutputStream("file.txt"));
-        // 8. Для работы с текстовыми файлами
-        FileReader fR = new FileReader("file.txt");
-        FileWriter fW = new FileWriter("file.txt",true);
-        // 9. Подкласс Writer, который используется для печати форматированных данных в OutputStream или другой Writer,
-        // которым он управляет. Все методы PrintWriter не выдают исключений I/O.
-        PrintWriter printW = new PrintWriter("file.txt");
+        Path path = Paths.get("A:\\TestFiles\\3file.txt");
+        path.getFileName();
+        path.getParent();
+        path.getFileSystem();
+        path.getNameCount();
+
+
+        // Проверяет, что файл доступен для чтения
+        Files.isReadable(Path.of("A:\\TestFiles\\3file.txt"));
+        // Проверяет, что файл доступен для записи
+        Files.isWritable(Path.of("A:\\TestFiles\\3file.txt"));
+        // Проверяет, что файл доступен для выполнения
+        Files.isExecutable(Path.of("A:\\TestFiles\\3file.txt"));
+        // Проверяет, что два пути указывают на один и тот же файл
+        String x = "A:\\TestFiles\\3file.txt";
+        Files.isSameFile(Path.of(x), Path.of(x));
+        System.out.println("wwwwww");
+        System.out.println(Files.isSameFile(Path.of("A:\\AllThems\\3file.txt"),Path.of(String.valueOf(Path.of("3file.txt").toRealPath()))));
+
 
     }
 }
